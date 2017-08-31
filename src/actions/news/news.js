464293644d1708f94f1049b865eb5ffcc5fee91b
Timur Asayonok <t.asaynok@gmail.com>
2017-08-31 08:21:01 +0300

    added caching data

--- src/actions/news/news.js
+++ src/actions/news/news.js
@@ -4,16 +4,22 @@ import { fetchNewsQuery } from '../graphql/queries'
 import * as Actions from '../../constants/actions'
 
 export function fetchNews(serviceName) {
-  return (dispatch) => {
+  return (dispatch, getState) => {
     dispatch({ type:Actions.FETCH_NEWS_START });
     client.query({
       query: fetchNewsQuery,
       variables: { serv: serviceName }
-    }).then((response) => {
+    })
+    .then((response) => {
       if(response.data){
         dispatch({ type: Actions.FETCH_NEWS_SUCCESS, payload: response.data });
       } else {
         dispatch({ type: Actions.FETCH_NEWS_ERROR });
+      }
+    })
+    .catch((error) => {
+      if (error.message === 'Network error: Network request failed'){
+        dispatch({ type: Actions.FETCH_NEWS_SUCCESS, payload: getState().news})
       }
     })
   }
